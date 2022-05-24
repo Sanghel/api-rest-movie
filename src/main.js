f625a68906ef04652ec3ad0926d7d36696d2ca47
Sanghel Gonzalez <sanghelgonzalez@gmail.com>
2022-05-24 18:53:08 -0400

    Preview of an Specific Movie

--- src/main.js
+++ src/main.js
@@ -11,12 +11,15 @@ const api = axios.create({
   
   // Utils
   
-  function createMovies(movies, container) {
+function createMovies(movies, container) {
     container.innerHTML = '';
   
     movies.forEach(movie => {
       const movieContainer = document.createElement('div');
       movieContainer.classList.add('movie-container');
+      movieContainer.addEventListener('click', () => {
+        location.hash = '#movie=' + movie.id;
+      });
   
       const movieImg = document.createElement('img');
       movieImg.classList.add('movie-img');
@@ -29,9 +32,9 @@ const api = axios.create({
       movieContainer.appendChild(movieImg);
       container.appendChild(movieContainer);
     });
-  }
+}
   
-  function createCategories(categories, container) {
+function createCategories(categories, container) {
     container.innerHTML = "";
   
     categories.forEach(category => {  
@@ -50,25 +53,25 @@ const api = axios.create({
       categoryContainer.appendChild(categoryTitle);
       container.appendChild(categoryContainer);
     });
-  }
+}
   
-  // Llamados a la API
+// Llamados a la API
   
-  async function getTrendingMoviesPreview() {
+async function getTrendingMoviesPreview() {
     const { data } = await api('trending/movie/day');
     const movies = data.results;
   
     createMovies(movies, trendingMoviesPreviewList);
-  }
+}
   
-  async function getCategegoriesPreview() {
+async function getCategegoriesPreview() {
     const { data } = await api('genre/movie/list');
     const categories = data.genres;
   
     createCategories(categories, categoriesPreviewList)  ;
-  }
+}
   
-  async function getMoviesByCategory(id) {
+async function getMoviesByCategory(id) {
     const { data } = await api('discover/movie', {
       params: {
         with_genres: id,
@@ -77,9 +80,9 @@ const api = axios.create({
     const movies = data.results;
   
     createMovies(movies, genericSection);
-  }
+}
   
-  async function getMoviesBySearch(query) {
+async function getMoviesBySearch(query) {
     const { data } = await api('search/movie', {
       params: {
         query,
@@ -88,11 +91,30 @@ const api = axios.create({
     const movies = data.results;
   
     createMovies(movies, genericSection);
-  }
+}
   
-  async function getTrendingMovies() {
+async function getTrendingMovies() {
     const { data } = await api('trending/movie/day');
     const movies = data.results;
   
     createMovies(movies, genericSection);
-  }
+}
+
+async function getMovieById(id) {
+    const { data: movie } = await api('movie/' + id);
+
+    const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+    headerSection.style.background = `
+    linear-gradient(
+        180deg, rgba(0, 0, 0, 0.35) 19.27%, 
+        rgba(0, 0, 0, 0) 29.17%
+    ),
+    url(${movieImgUrl})
+    `;
+  
+    movieDetailTitle.textContent = movie.title;
+    movieDetailDescription.textContent = movie.overview;
+    movieDetailScore.textContent = movie.vote_average;
+
+    createCategories(movie.genres, movieDetailCategoriesList);
+}
--- src/navigation.js
+++ src/navigation.js
@@ -93,6 +93,10 @@ searchFormBtn.addEventListener('click', () => {
     categoriesPreviewSection.classList.add('inactive');
     genericSection.classList.add('inactive');
     movieDetailSection.classList.remove('inactive');
+
+    // ['#movie', '28971']
+    const [_, movieId] = location.hash.split('=');
+    getMovieById(movieId);
   }
   
   function searchPage() {
